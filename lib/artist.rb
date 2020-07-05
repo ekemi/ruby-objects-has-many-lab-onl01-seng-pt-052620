f61e367a19d95fd45d578ea3d31918d3b965489a
edmundo papi <edmundopierre@yahoo.com>
2020-07-05 10:59:11 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+     #binding.pry
+  end
+  def self.all
+    @@all
+
+  end
+  def songs
+ Song.all.select{|song| song.artist = self}
+ #binding.pry
+  end
+
+  def add_song(song)
+
+  song.artist = self
+  end
+def add_song_by_name(song_name)
+  song = Song.new(song_name)
+  add_song(song)
+end
+def self.song_count
+Song.all.count
+#binding.pry
+end
+end
---
+++ lib/author.rb
@@ -0,0 +1,22 @@
+class Author
+
+  attr_accessor :name, :post
+
+def initialize(name)
+  @name = name
+end
+
+def post
+
+end
+def add_post(arg_post)
+
+end
+def add_post_by_title
+
+end
+
+def post_count
+
+end
+end
